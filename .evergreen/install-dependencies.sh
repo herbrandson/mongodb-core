faa1abdd39dec56a1222548ccd6d0f1ed3187106
Matt Broadstone <mbroadst@gmail.com>
2018-03-12 21:50:55 -0400

    gettin creative

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -25,6 +25,40 @@ init-module=${NPM_CACHE_DIR}/.npm-init.js
 cache=${NPM_CACHE_DIR}
 EOT
 
+
+cat <<EOT > index.js
+var url = require('url');;
+var path = require('path');;
+
+module.exports = cf;;
+
+function cf(root, u) {
+  console.log('ROOT: ', root);
+  console.log('U: ', u);
+
+  if (!u)
+    return cf.bind(null, root);;
+
+  u = url.parse(u);;
+  var h = u.host.replace(/:/g, '_');;
+  // Strip off any /-rev/... or ?rev=... bits
+  var revre = /(\?rev=|\?.*?&rev=|\/-rev\/).*$/;;
+  var parts = u.path.replace(revre, '').split('/').slice(1);;
+  // Make sure different git references get different folders
+  if (u.hash && u.hash.length > 1) {
+    parts.push(u.hash.slice(1));;
+  };;
+  var p = [root, h].concat(parts.map(function(part) {
+    return encodeURIComponent(part).replace(/%/g, '_');;
+  }));;
+
+  console.log('FINAL: ', path.join.apply(path, p));
+  return path.join.apply(path, p);;
+}
+EOT
+
+mv index.js ${NVM_DIR}/versions/node/v4.8.7/lib/node_modules/npm/node_modules/npm-cache-filename/index.js
+
 # install node dependencies
 # npm install --unsafe-perm=true --allow-root
 
