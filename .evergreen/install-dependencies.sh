decbf97485a9498680e207db05313928ff2ebafd
Matt Broadstone <mbroadst@gmail.com>
2018-03-12 22:10:26 -0400

    final creative solution

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -28,55 +28,5 @@ cache=${NPM_CACHE_DIR}
 tmp=${NPM_TMP_DIR}
 EOT
 
-cat <<EOT > index.js
-var url = require('url');;
-var path = require('path');;
-
-module.exports = cf;;
-
-function cf(root, u) {
-  console.log('ROOT: ', root);
-  console.log('U: ', u);
-
-  if (!u)
-    return cf.bind(null, root);;
-
-  u = url.parse(u);;
-  var h = u.host.replace(/:/g, '_');;
-  // Strip off any /-rev/... or ?rev=... bits
-  var revre = /(\?rev=|\?.*?&rev=|\/-rev\/).*$/;;
-  var parts = u.path.replace(revre, '').split('/').slice(1);;
-  // Make sure different git references get different folders
-  if (u.hash && u.hash.length > 1) {
-    parts.push(u.hash.slice(1));;
-  };;
-  var p = [root, h].concat(parts.map(function(part) {
-    return encodeURIComponent(part).replace(/%/g, '_');;
-  }));;
-
-  console.log('FINAL: ', path.join.apply(path, p));
-  return path.join.apply(path, p);;
-}
-EOT
-
-mv index.js ${NVM_DIR}/versions/node/v4.8.7/lib/node_modules/npm/node_modules/npm-cache-filename/index.js
-
 # install node dependencies
-# npm install --unsafe-perm=true --allow-root
-
-# npm install bson
-npm install chai --verbose
-
-# npm install require_optional
-# npm install co
-# npm install conventional-changelog-cli
-# npm install eslint
-# npm install eslint-plugin-prettier
-# npm install jsdoc
-# npm install mongodb-mock-server
-# npm install prettier
-# npm install snappy
-# npm install kerberos
-
-# npm install mongodb-test-runner
-
+npm install
